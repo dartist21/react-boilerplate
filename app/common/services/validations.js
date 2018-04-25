cbfc633efe70b9ac4828a123367c4fb108c0d711
Denys <dartist21@gmail.com>
2018-04-25 14:27:41 +0300

    fix: PATTERNS_IMAGE_URL

--- app/common/services/validations.js
+++ app/common/services/validations.js
@@ -1,7 +1,7 @@
 import { addValidation } from 'react-nebo15-validate';
 
 addValidation('imageUrl', (value) => {
-  const PATTERNS_IMAGE_URL = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/;
+  const PATTERNS_IMAGE_URL = /(http(s?):)(.*)\.(?:jpg|gif|png)/i;
   return PATTERNS_IMAGE_URL.test(value);
 });
 
