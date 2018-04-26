c662b816efea3affb4cdd01e52cb38e9f3112826
Denys <dartist21@gmail.com>
2018-04-27 00:54:28 +0300

    fix: checking_favorite test

--- cypress/integration/checking_favorite.js
+++ cypress/integration/checking_favorite.js
@@ -1,21 +1,39 @@
 describe('Checking favorite film test', () => {
-  it('Checking favorite film', () => {
-    cy.request('/api/movies').then((resp) => {
-      const favoriteFilm = resp.body.data.find(f => f.is_favorite === true);
+  beforeEach(() => {
+    cy.fixture('movie').as('movie');
+  });
 
-      if (!favoriteFilm) {
-        throw new Error('You don t have favorite film');
-      }
+  it('Checking favorite film', function () {
+    cy.visit('/movies/');
 
-      cy.visit('/movies/');
-      cy.contains(favoriteFilm.title).within(() => {
-        cy.get('span > i');
-      });
+    cy
+      .get('div[class*=MovieCard]')
+      .first()
+      .click();
 
-      cy.visit(`/movies/${favoriteFilm.id}`);
-      cy.contains(favoriteFilm.title).within(() => {
-        cy.get('span > i');
-      });
+    cy.contains('Edit movie').click();
+
+    cy
+      .get('input[name=title]')
+      .clear()
+      .type(this.movie.title)
+      .should('have.value', this.movie.title);
+
+    cy
+      .get('input[id=is_favorite]')
+      .check()
+      .should('be.checked');
+
+    cy.get('button').click();
+
+    cy.contains(this.movie.title).within(() => {
+      cy.get('span > i');
+    });
+
+    cy.visit('/movies/');
+
+    cy.contains(this.movie.title).within(() => {
+      cy.get('span > i');
     });
   });
 });
